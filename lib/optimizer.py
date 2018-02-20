f77b83d2662c4356bb241d0b101e3818cfe272bf
watamacha <36643613+watamacha@users.noreply.github.com>
2018-02-19 23:45:37 -0600

    visualization and debug
    code now runs and opens a graph containing all coordinates, however the stepsize is too small to change the x value so we need to implement axis scaling.

--- lib/optimizer.py
+++ lib/optimizer.py
@@ -13,20 +13,23 @@ data = [[0,1.0000000],
 
 
 #define functions to optimize
-def f1(n,r):
-    return math.exp(r*n)
-def f2(n,s,g,c):
+def f1(n,params):
+    return math.exp(n*params[0])
+def f2(n,params):
+    s = params[0]
+    g = params[1]
+    c = params[3]
     return (s**n)*(g**((c**n) - 1))
 
 #define inverse value function, sum of error squares to be minimized
 def v(funct, params):
     sum = 0
-    for i in xrange(10):
+    for i in range(10):
         sum += (funct(data[i][0],params) - data[i][1])**2
     return sum
 
 #define lower and upper bounds for parameters. Write them into a 2xn matrix
-lbr = -0.01
+lbr = 0-0.01
 ubr = 0
 
 lbc = 1.07
@@ -55,13 +58,14 @@ f2bounds = [[lbs,ubs],
 
 
 
-def bruteSearch(funct, iterations, *parameterBounds):
+def bruteSearch(funct, iterations, parameterBounds):
     #initialize a coordinate variable, as well as a value list for graphing. here the last thing in pc is the return of the value function and the rest is an ordered list of parameter values
     #values is not gonna be ordered since ordering in higher dimensions is an issue we can deal with afterwards, and if we have enough points to be troublesome to sort for line graphing 
     #they'll be dense enough not to worry about it and just use a scatter
     edge = 2*iterations + 1
     df = len(parameterBounds)
-    pc = [0 for x in xrange(df)].extend(0)
+    pc = [0 for x in range(df)]
+    pc.append(0)
     best = pc
     pcx = pc
     pcx.pop()
@@ -69,10 +73,12 @@ def bruteSearch(funct, iterations, *parameterBounds):
     #next we define some useful information for the iterator
     #the iteration works by moving by stepsize in each direction using modular stuff
     stepsize = pcx
-    for p in xrange(df):
-        stepsize[p] = (parameterBounds[p][1]-parameterBounds[p][0])/(2**iterations)
+    for p in range(df):
+        lb = parameterBounds[p][0]
+        ub = parameterBounds[p][1]
+        stepsize[p] = (ub-lb)/(2**iterations)
     #now we initialize pc to the first coordinate it must check
-    for p in xrange(df):
+    for p in range(df):
         #ayyy james brown hows it goin
         pc[p] = (parameterBounds[p][0])
         pcx[p] = pc[p]
@@ -80,10 +86,11 @@ def bruteSearch(funct, iterations, *parameterBounds):
     #here we iterate
     #our index goes up to 2i-1^df, since thats how many points we want
     for index in range(edge**df):
-        for d in df:
+        for d in range(df):
             #every edge times the previous d goes up, this one goes up. done with floor division.
-            pcx[d] = stepsize[d] * (index//(edge**d))
-        pc = pcx.append(v(funct,pcx))
+            pcx[d] = stepsize[d] * (index//(edge**(d+1)))
+        pc = pcx
+        pc.append(v(funct,pcx))
         values[index] = pc
         if pc[df] > best[df]:
             best = pc
@@ -93,3 +100,5 @@ def bruteSearch(funct, iterations, *parameterBounds):
     #effectively we include the length information of 'best' implicitly in the structure of the return array, which also aids in visualization construction.
     values.append(best)
     return values
+
+
--- visualize.py
+++ visualize.py
@@ -1,1 +1,17 @@
-import optimizer
+from lib import optimizer as op
+
+import matplotlib.pyplot as plt
+
+
+values = op.bruteSearch(op.f1,50,op.f1bounds)
+x = [values[z][0] for z in range(len(values)-1)]
+y = [values[z][1] for z in range(len(values)-1)]
+
+
+
+
+print(x)
+plt.plot(x,y, 'ro')
+plt.ylabel('value')
+plt.xlabel('n')
+plt.show()
