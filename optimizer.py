30e70e39c3faa22594daa33a2204022590a18e40
watamacha <36643613+watamacha@users.noreply.github.com>
2018-02-19 20:09:51 -0600

    finished brute optimizer code
    ez pz

--- optimizer.py
+++ optimizer.py
@@ -43,34 +43,53 @@ f1bounds = [[lbr,ubr]]
 f2bounds = [[lbs,ubs],
             [lbg,ubg],
             [lbc,ubc]]
+
+
+
 #define a brute force by subdividing the boundaries into halves k times
+
+
+
+
+
+
+
+
 def bruteSearch(funct, iterations, *parameterBounds):
     #initialize a coordinate variable, as well as a value list for graphing. here the last thing in pc is the return of the value function and the rest is an ordered list of parameter values
-    pc = [0 for x in xrange(len(parameterBounds))].extend(0)
-    values = [pc for x in range(2**iterations - 1)]
+    #values is not gonna be ordered since ordering in higher dimensions is an issue we can deal with afterwards, and if we have enough points to be troublesome to sort for line graphing 
+    #they'll be dense enough not to worry about it and just use a scatter
+    edge = 2*iterations + 1
+    df = len(parameterBounds)
+    pc = [0 for x in xrange(df)].extend(0)
     best = pc
-    
+    pcx = pc
+    pcx.pop()
+    values = [pc for x in range(edge**df)]
     #next we define some useful information for the iterator
-    #the iteration works by starting from the center, then moving by boundarysize/2^iterations in each direction. This allows for the omission of boundary conditional statements
-    stepsize = pc
-    steps.pop()
-
-
-
-
+    #the iteration works by moving by stepsize in each direction using modular stuff
+    stepsize = pcx
+    for p in xrange(df):
+        stepsize[p] = (parameterBounds[p][1]-parameterBounds[p][0])/(2**iterations)
+    #now we initialize pc to the first coordinate it must check
+    for p in xrange(df):
+        #ayyy james brown hows it goin
+        pc[p] = (parameterBounds[p][0])
+        pcx[p] = pc[p]
 
     #here we iterate
-    for iteration in xrange(iterations):
-    
-    
-        
-        
-        
-        
-        
-        
-    #here we return our coordinates, along with the best coordinate. note that append is used instead of extend here.
-    #this is because extend stores the entire best array in the last index of values, so values(len(values)-1) is always best, regardless of the size of best.
-    #effectively we include the length information of best implicitly in the structure of the return array
-   values.append(best)
-   return values
+    #our index goes up to 2i-1^df, since thats how many points we want
+    for index in range(edge**df):
+        for d in df:
+            #every edge times the previous d goes up, this one goes up. done with floor division.
+            pcx[d] = stepsize[d] * (index//(edge**d))
+        pc = pcx.append(v(funct,pcx))
+        values[index] = pc
+        if pc[df] > best[df]:
+            best = pc
+           
+    #here we return our coordinates, along with the 'best' coordinate. note that append is used instead of extend here.
+    #this is because extend stores the entire 'best' array in the last index of values, so values(len(values)-1) is always 'best', regardless of the size of 'best.'
+    #effectively we include the length information of 'best' implicitly in the structure of the return array, which also aids in visualization construction.
+    values.append(best)
+    return values
