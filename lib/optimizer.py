e52c1242a9d1bde0e5a5ea463b8e16f37704e60b
watamacha <36643613+watamacha@users.noreply.github.com>
2018-02-21 14:19:47 -0600

    reimplemented brute search
    added better comments and console outputs, should be better suited for partial rewrites to produce new optimization functions.

--- lib/optimizer.py
+++ lib/optimizer.py
@@ -21,25 +21,25 @@ def f2(n,params):
     c = params[3]
     return (s**n)*(g**((c**n) - 1))
 
-#define inverse value function, sum of error squares to be minimized
+#define value function, sum of error squares. must be minimized.
 def v(funct, params):
-    sum = 0
+    sum = 0.
     for i in range(10):
         sum += (funct(data[i][0],params) - data[i][1])**2
     return sum
 
 #define lower and upper bounds for parameters. Write them into a 2xn matrix
 lbr = 0-0.01
-ubr = 0
+ubr = 0.
 
 lbc = 1.07
 ubc = 1.13
 
 lbs = 0.996
-ubs = 1
+ubs = 1.
 
 lbg = 0.98
-ubg = 1
+ubg = 1.
 
 f1bounds = [[lbr,ubr]]
 
@@ -58,7 +58,7 @@ f2bounds = [[lbs,ubs],
 
 
 
-def bruteSearch(funct, iterations, parameterBounds):
+def bruteSearchold(funct, iterations, parameterBounds):
     #initialize a coordinate variable, as well as a value list for graphing. here the last thing in pc is the return of the value function and the rest is an ordered list of parameter values
     #values is not gonna be ordered since ordering in higher dimensions is an issue we can deal with afterwards, and if we have enough points to be troublesome to sort for line graphing 
     #they'll be dense enough not to worry about it and just use a scatter
@@ -101,4 +101,100 @@ def bruteSearch(funct, iterations, parameterBounds):
     values.append(best)
     return values
 
+def bruteSearch(funct, stepsperparam, parameterBounds):
+    ###INPUT DEFINITION:
+    #funct should be a function with args (n, params), where params is excepted to be the same size as parameterBounds. had issues with isinstance(x, function)
+    if not True:
+        print("ERROR: funct is not a function")
+        return []
+    #stepsperparam should be an integer greater than 0.
+    if not isinstance(stepsperparam, int):
+        print ("ERROR: stepsperparam is not an int")
+        return []
+    if stepsperparam < 1:
+        print ("ERROR: invalid number of steps per parameter")
+        return []
+    #parameterBounds can be a list of any size, and each element must be a list of size 2 containing 2 ordered floats.
+    if not isinstance(parameterBounds, list):
+        print("ERROR: parameterBounds is not a list")
+        return []
+    for x in parameterBounds:
+        if len(x) != 2:
+            print ("ERROR: invalid length member of parameterBounds")
+            return []
+        if not isinstance(x[0],float):
+            print("ERROR: one of the lower bounds is not a float")
+            return []
+        if not isinstance(x[1],float):
+            print("ERROR: one of the upper bounds is not a float")
+            return []
+        if x[0] >= x[1]:
+            print("ERROR:invalid parameter boundary size")
+            return []
+    #in our case, (fk, any integer greater than 0, fkbounds) is a valid input set if k is 1 or 2.
+    ###END INPUT DEFINITION
+    
+    ###LOCALS DEFINITION:
+    #floats should be used for any non-indexing variables. ints/longs (handled automatically by dynamic typing) for any indexing variables.
+    #define df, the number of parameters to optimize over
+    df = len(parameterBounds)
+    print("degrees of freedom: ", df)
+    #define co, the current parameter values (a coordinate in our parameter space)
+    co = [0. for x in range(df)]
+    #define ord, the evaluation of the value function at our current parameter value set (the value of a point in our value field)
+    ord = 0.
+    #define coord, an array that defines a point in our value field (field in the parameterbounds bounded subset of the parameter space)
+    #must be 2 lines since append method does not return the result of the append.
+    coord = [0. for x in co]
+    coord.append(ord)
+    print(co)
+    #define our coords array, which is just an array of known coordinates, as well as bco, bord, and bcoord, which are the co, ord, and coord for the current best
+    #since we aim to minimize, we initialize bord to a high enough value that we can find something below it.
+    values = []
+    bco = co
+    bord = 999.
+    bcoord = bco
+    bcoord.append(bord)
+    #next we calculate stepsizes using lower and upper bounds
+    stepsize = [(parameterBounds[p][1]-parameterBounds[p][0])/(stepsperparam) for p in range(df)]
+    print("stepsize array: ", stepsize)
+    #we also define sco, the same as co but encoded as integer number of steps
+    sco = [0 for x in co]
+    #next we define our startpoint as the minimum of all parameters
+    co = [x[0] for x in parameterBounds]
+    #we define a function to cleanly update ord and coord given co
+    def up(ico):
+        ord = v(funct, ico)
+        coord = ico
+        coord.append(ord)
+    #now we define the number of points we will evaluate
+    points = (stepsperparam + 1)**df
+    ###END LOCALS DEFINITION
+
+
+    ###FUNCTION DEFINITION:
+    #now we begin iterating.
+    #from our start coordinate, we move p1 up every step, and reset it every stepsperparam steps.
+    #we move p[k] up every stepsperparam^(k-1) steps, and reset it every stepsperparam^k steps
+    #incrementing i at every j increments of k is done by i = k//j (floor division)
+    #resetting i at every j increments of k is done by i = k%j
+    #result is (i//stepsperparam**(p-1))%(stepsperparam**p)
+    for i in range(points):
+        sco = [i for p in range(df)]
+        #co = [stepsize[p] * sco[p] for p in range(df)]
+        co = [(sco[0]+0) * (stepsize[0]+0.0)]
+        up(co)
+        print("evaluated ", sco, co, ord, coord)
+        values.append(coord)
+        if ord < bord:
+            bco = co
+            bord = ord
+            bcoord = coord
+            print("found new optimum of ", bord, " at ", bco)
+    values.append(bcoord)
+    ###END FUNCTION DEFINITION
+
+    ###RETURN DEFINITION:
+    return values
+    ###END RETURN DEFINITION
 
--- visualize.py
+++ visualize.py
@@ -3,14 +3,14 @@ from lib import optimizer as op
 import matplotlib.pyplot as plt
 
 
-values = op.bruteSearch(op.f1,50,op.f1bounds)
+values = op.bruteSearch(op.f1,5,op.f1bounds)
 x = [values[z][0] for z in range(len(values)-1)]
 y = [values[z][1] for z in range(len(values)-1)]
 
 
 
-
-print(x)
+print("x: ", x)
+print("y: ", y)
 plt.plot(x,y, 'ro')
 plt.ylabel('value')
 plt.xlabel('n')
