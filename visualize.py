8fddfc8cbbaaab99e7eed730c7cd0380d7411078
watamacha <36643613+watamacha@users.noreply.github.com>
2018-02-23 12:47:57 -0600

    fixed stuff

--- visualize.py
+++ visualize.py
@@ -11,15 +11,30 @@ by = y.pop()
 
 plt.plot(x ,y, 'r:', bx, by, "go")
 plt.ylabel('value')
-plt.xlabel('parameter ' + '0' )
+plt.xlabel('parameter r' )
 plt.show()
 
-values = op.bruteSearch(op.f2,100,op.f2bounds)
+values = op.bruteSearch(op.f2,50,op.f2bounds)
 best = values.pop()
-cbgbc = filter(lambda x: x[1] == best[1] && x[2] = best[2], values)
+cbgbc = list(filter(lambda x: x[1] == best[1] and x[2] == best[2], values))
 sbgbc = [c[0] for c in cbgbc]
 vbgbc = [c[3] for c in cbgbc]
 plt.plot(sbgbc , vbgbc, 'r:', best[0], best[3], "go")
 plt.ylabel('value')
-plt.xlabel('s given best g, best c')
+plt.xlabel('parameter s given best g, best c')
+plt.show()
+
+cbgbc = list(filter(lambda x: x[0] == best[0] and x[2] == best[2], values))
+sbgbc = [c[1] for c in cbgbc]
+vbgbc = [c[3] for c in cbgbc]
+plt.plot(sbgbc , vbgbc, 'r:', best[1], best[3], "go")
+plt.ylabel('value')
+plt.xlabel('parameter g given best s, best c')
+plt.show()
+cbgbc = list(filter(lambda x: x[0] == best[0] and x[1] == best[1], values))
+sbgbc = [c[2] for c in cbgbc]
+vbgbc = [c[3] for c in cbgbc]
+plt.plot(sbgbc , vbgbc, 'r:', best[2], best[3], "go")
+plt.ylabel('value')
+plt.xlabel('parameter c given best s, best g')
 plt.show()
