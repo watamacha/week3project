308ee3371ccb8b0290ea8c6d827c9a50457c9bc0
watamacha <36643613+watamacha@users.noreply.github.com>
2018-02-23 11:44:09 -0600

    added graph of value versus s given best g and c

--- visualize.py
+++ visualize.py
@@ -3,14 +3,23 @@ from lib import optimizer as op
 import matplotlib.pyplot as plt
 
 
-values = op.bruteSearch(op.f2,30,op.f2bounds)
-x = [[values[z][k] for z in range(len(values)-1)] for k in range(len(values[0])-1)]
-y = [values[z][len(x)] for z in range(len(values)-1)]
-bx = [values[len(values)-1][k] for k in range(len(values[0])-1)]
-by = values[len(values)-1][len(bx)]
+values = op.bruteSearch(op.f1,10000,op.f1bounds)
+x = [v[0] for v in values]
+bx = x.pop()
+y = [v[1] for v in values]
+by = y.pop()
 
-for p in range(len(bx)):
-    plt.plot(x[p] ,y, 'r:', bx[p], by, "go")
-    plt.ylabel('value')
-    plt.xlabel('parameter ' + str(p) )
-    plt.show()
+plt.plot(x ,y, 'r:', bx, by, "go")
+plt.ylabel('value')
+plt.xlabel('parameter ' + '0' )
+plt.show()
+
+values = op.bruteSearch(op.f2,100,op.f2bounds)
+best = values.pop()
+cbgbc = filter(lambda x: x[1] == best[1] && x[2] = best[2], values)
+sbgbc = [c[0] for c in cbgbc]
+vbgbc = [c[3] for c in cbgbc]
+plt.plot(sbgbc , vbgbc, 'r:', best[0], best[3], "go")
+plt.ylabel('value')
+plt.xlabel('s given best g, best c')
+plt.show()
