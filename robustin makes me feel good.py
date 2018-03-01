644f1bf0b78b15e7f9dac915837a1e37f799d09f
watamacha <36643613+watamacha@users.noreply.github.com>
2018-03-01 12:34:50 -0600

    added sdevs

--- robustin makes me feel good.py
+++ robustin makes me feel good.py
@@ -1,5 +1,6 @@
 from lib import optimizer as op
 import matplotlib.pyplot as plt
+import statistics
 dataset = [ [0,1.0000000],
             [15,.9743175],
             [25,.9575636],
@@ -32,20 +33,25 @@ for searchindex in range(2**10 - 1):
     rety[len(data)].append(retval[1])
 
 mu = [0 for j in range(9)]
+sd = [0 for j in range(9)]
 for j in range(9):
     i = j+1
     plt.title("distribution of optimums with data subset of size " + str(i))
     plt.hist(retx[i],1+(len(retx[i])//2),histtype='stepfilled',cumulative = False)#,weights=[1/y for y in rety[i]])
-    for x in retx[i]:
-        mu[j] += x
-    mu[j] /= len(retx[i])
-    plt.axvline(best[0],color='green')
+    mu[j] = statistics.mean(retx[i])
+    sd[j] = statistics.stdev(retx[i])
+    plt.axvline(best[0],color='black')
     plt.axvline(mu[j],color='red')
+    plt.axvline(mu[j]-sd[j],color='yellow')
+    plt.axvline(mu[j]+sd[j],color='yellow')
+    plt.axvline()
     plt.show()
 
 mu.append(best[0])
+sd.append(0)
 plt.title('mean of optimums versus sample subset size')
-plt.plot(range(10),mu,'r-',range(10),mu,'bo')
-plt.axhline(best[0],color='green')
+plt.plot(range(10),mu,'b-',range(10),mu,'ro')
+plt.errorbar(range(10),mu,yerr=sd,fmt='none',ecolor='yellow')
+plt.axhline(best[0],color='black')
 plt.show()
 
